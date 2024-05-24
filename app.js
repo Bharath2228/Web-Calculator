9c43e6c851c8e2787aafbc18f96e5c16e1da1ed6
Bharath2228 <pbharath2210@gmail.com>
2024-05-24 17:44:39 +0530

    calcualtion is done using eval functionality

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
+const display = document.getElementById('number-el')
 
+let values = []
+
+function append(input){
+    display.textContent += input;
+    values = Array.from(display.textContent)
+}
+
+function calculate(){
+
+    try{
+        display.textContent = eval(display.textContent)
+    }
+    catch(error){
+        display.textContent = "Error"
+    }
+    
+}
+
+function removeele(){
+     values.pop()
+     display.textContent = values.join('')
+}
+
+function clearDisp(){
+    display.textContent = "";
+    values = []
+}
