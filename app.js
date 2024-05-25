3b561660440496eed0e2148c10b624c7d3d04eae
Bharath2228 <pbharath2210@gmail.com>
2024-05-25 11:57:30 +0530

    able to fetch the data of each key using EventListner

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-const display = document.getElementById('number-el')
+const keys = document.querySelectorAll('.key')
+const displayInput = document.querySelector(".display .input")
+const displayOutput = document.querySelector(".display .output")
 
-let values = []
+let input = "";
 
-function append(input){
-    display.textContent += input;
-    values = Array.from(display.textContent)
-}
-
-function calculate(){
-
-    try{
-        display.textContent = eval(display.textContent)
-    }
-    catch(error){
-        display.textContent = "Error"
-    }
+keys.forEach(key => {
+    const value = key.dataset.key;
     
-}
+    key.addEventListener('click', () => {
+        if (value == "clear"){
+            input = "";
+            displayInput.innerHTML = "";
+            displayOutput.innerHTML = "";
+        }else if(value == "backspace"){
+            input = input.slice(0, -1)
+            displayInput.innerHTML = "input";
+        }else if(value == "="){
+            let result = eval(input);  // gonna change it later
+            displayOutput.innerHTML = result;
+        }else if(value = "brackets"){
+            if(input.indexOf("(") == -1 || 
+               input.indexOf("(") != -1 && 
+               input.indexOf(")") != -1 && 
+               input.lastIndexOf("(") > input.lastIndexOf(")")){
+                input += "("
+            } 
+        }
+    }) 
 
-function removeele(){
-     values.pop()
-     display.textContent = values.join('')
-}
-
-function clearDisp(){
-    display.textContent = "";
-    values = []
-}
+    }
+})
