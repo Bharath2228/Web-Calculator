43d0a789dbb502aa7e17e44f62a61eb99cf2b8b8
Bharath2228 <pbharath2210@gmail.com>
2024-05-27 20:41:07 +0530

    Resolving all the edge cases for input and output display

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const displayOutput = document.querySelector(".display .output")
 let input = "";
 
 keys.forEach(key => {
-    const value = key.dataset.key;
+    let value = key.dataset.key;
     
     key.addEventListener('click', () => {
         if (value == "clear"){
@@ -14,19 +14,107 @@ keys.forEach(key => {
             displayOutput.innerHTML = "";
         }else if(value == "backspace"){
             input = input.slice(0, -1)
-            displayInput.innerHTML = "input";
+            displayInput.innerHTML = cleanInput(input);
         }else if(value == "="){
-            let result = eval(input);  // gonna change it later
-            displayOutput.innerHTML = result;
-        }else if(value = "brackets"){
+            let result = eval(PrepareInput(input));  // gonna change it later
+            displayOutput.innerHTML = cleanOutput(result);
+
+        }else if(value == "brackets"){
             if(input.indexOf("(") == -1 || 
                input.indexOf("(") != -1 && 
                input.indexOf(")") != -1 && 
-               input.lastIndexOf("(") > input.lastIndexOf(")")){
-                input += "("
+               input.lastIndexOf("(") < input.lastIndexOf(")")){
+                input += "(";
+            } else if(input.indexOf("(") != -1 && 
+                      input.indexOf(")") == -1 || 
+                      input.indexOf("(") != -1 && 
+                      input.indexOf(")") != -1 && 
+                      input.lastIndexOf("(") > input.lastIndexOf(")")){
+                input += ")";
             } 
+
+            displayInput.innerHTML = cleanInput(input);
+        } else{
+            if (ValidateInput(value)){
+                input += value;
+                displayInput.innerHTML = cleanInput(input);
+            }
         }
     }) 
-
     }
-})
+)
+
+function cleanInput(input){
+    let input_array = input.split("")
+
+    for(let i = 0; i < input_array.length; i++){
+        if (input_array[i] == '*'){
+            input_array[i] = `<span class="operator">x</span>`;
+        } else if(input_array[i] == '/'){
+            input_array[i] = `<span class="operator">÷</span>`;
+        } else if (input_array[i] == '-'){
+            input_array[i] = `<span class="operator">-</span>`;
+        } else if(input_array[i] == '+'){
+            input_array[i] = `<span class="operator">+</span>`;
+        }else if (input_array[i] == '('){
+            input_array[i] = `<span class="brackets">(</span>`;
+        } else if (input_array[i] == ')'){
+            input_array[i] = `<span class="brackets">)</span>`;
+        } else if(input_array[i] == '%'){
+            input_array[i] = `<span class = "percent">%</span>`;
+        }
+    }
+
+    return input_array.join('')
+}
+
+function cleanOutput(output){
+    let output_string = output.toString();
+    let decimal = output_string.split(".")[1];
+    output_string = output_string.split(".")[0];
+
+    output_array = output_string.split("")
+
+    if (output_array.length > 3){
+        for (let i = output_array.length - 3; i > 0; i -= 3){
+            output_array.splice(i, 0, ",")
+        }
+    }
+
+    if (decimal) {
+        output_array.push(".");
+        output_array.push(decimal);
+    }
+
+    return output_array.join("");
+}
+
+function PrepareInput(input){
+    let input_array = input.split("")
+
+    for (let i = 0; i < input_array.length; i++){
+        if(input_array[i] == "%"){
+            input_array[i] == "/100"
+        }
+    }
+
+    return input_array.join("")
+}
+
+function ValidateInput(value){
+    let last_input = input.slice(-1)
+    let operators = ["+", "-", "*", "/"]
+
+    if(value == "." && last_input == "."){
+        return false
+    }
+
+    if (operators.includes(value)){
+        if (operators.includes(last_input)){
+            return false;
+        }
+        return true;
+    }
+
+    return true;
+}
